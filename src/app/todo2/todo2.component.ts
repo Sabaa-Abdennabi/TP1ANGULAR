e60131a0e7da915177bb7bd923457dc1c5764b70
Sabaa-Abdennabi <saba.abdennebi@insat.ucar.tn>
2024-11-09 18:24:58 +0100

    starting todo2

---
+++ src/app/todo2/todo2.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-todo2',
+  standalone: true,
+  imports: [],
+  templateUrl: './todo2.component.html',
+  styleUrl: './todo2.component.css'
+})
+export class Todo2Component {
+
+}
