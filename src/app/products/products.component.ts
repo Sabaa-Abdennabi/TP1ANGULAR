101f23a9f20e2f70c09ef7c6f6bb1634d64c374f
Sabaa-Abdennabi <saba.abdennebi@insat.ucar.tn>
2024-11-26 00:00:44 +0100

    qddedd products

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, inject } from '@angular/core';
 import {
   BehaviorSubject,
   Observable,
@@ -6,19 +6,47 @@ import {
   map,
   takeWhile,
   scan,
-} from "rxjs";
-import { Product } from "./dto/product.dto";
-import { ProductService } from "./services/product.service";
-import { Settings } from "./dto/product-settings.dto";
+  take,
+  tap,
+} from 'rxjs';
+import { Product } from './dto/product.dto';
+import { ProductService } from './services/product.service';
+import { Settings } from './dto/product-settings.dto';
+import { ProductApiResponse } from './dto/product-api-response.dto';
 
 @Component({
-  selector: "app-products",
-  templateUrl: "./products.component.html",
-  styleUrls: ["./products.component.css"],
+  selector: 'app-products',
+  templateUrl: './products.component.html',
+  styleUrls: ['./products.component.css'],
 })
 export class ProductsComponent {
   /* Todo : Faire le nécessaire pour créer le flux des produits à afficher */
   /* Tips : vous pouvez voir les différents imports non utilisés et vous en inspirer */
+  productsResult$!: Observable<ProductApiResponse>;
   products$!: Observable<Product[]>;
-  constructor() {}
+  productService = inject(ProductService);
+  private loadMore = new BehaviorSubject<{ limit: number; skip: number }>({
+    limit: 12,
+    skip: 0,
+  });
+  private allloaded = false;
+  constructor() {
+    this.products$ = this.loadMore.pipe(
+      takeWhile(() => !this.allloaded),
+      concatMap(({ limit, skip }) =>
+        this.productService.getProducts({ limit, skip }).pipe(
+          tap((response) => {
+            if (response.products.length < limit) {
+              this.allloaded = true;
+            }
+          })
+        )
+      ),
+      scan((acc, response) => [...acc, ...response.products], [] as Product[])
+    );
+  }
+  loadMoreProducts() {
+    const current = this.loadMore.value;
+    this.loadMore.next({ limit: current.limit, skip: current.skip + current.limit });
+  }
 }
